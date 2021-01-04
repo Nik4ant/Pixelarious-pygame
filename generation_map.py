8864f2333e669e8130acf3f8119daa901e5400a0
Massering <74819874+Massering@users.noreply.github.com>
2021-01-04 15:09:24 +0700

    Add files via upload

--- generation_map.py
+++ generation_map.py
@@ -1,4 +1,4 @@
-from random import *
+from random import choice
 import os
 import pygame
 import sys
@@ -46,7 +46,7 @@ def generate_level(level):
                 Tile('.', x, y)
                 new_player = Player(x, y)
             elif level[y][x] == '.':
-                Tile(choice(list('.....') + ['.0', '.1', '.2', '.3']), x, y)
+                Tile(choice(list('.' * 15) + ['.0', '.1', '.2', '.3']), x, y)
             else:
                 Tile(level[y][x], x, y)
     # вернем игрока
@@ -76,7 +76,7 @@ class Player(pygame.sprite.Sprite):
         self.last_move = time() - 1
 
     def move(self, keys):
-        if time() - self.last_move < 0.1:
+        if time() - self.last_move < 0.02:
             return
         x, y = self.pos
         if keys[4] or keys[80]:
@@ -188,15 +188,15 @@ D.........D
 
 EVIL_ROOM_2 = '''
 43338D63332
-5.........1
-5.M...C...1
-5.433333331
-8.677777776
+5.......MC1
+5.433333336
+5.677777772
+8.........6
 D.........D
-333333332.4
+2.........4
+633333332.1
 477777778.1
-5...C..M..1
-5.........1
+5CM.......1
 67772D47778
 '''
 
@@ -217,13 +217,13 @@ D..M.C.M..D
 EVIL_ROOM_4 = '''
 43338D63332
 5.........1
-5......M..1
-5.........1
+62.....M.48
+48.......62
 8.........6
 D.........D
 2.........4
-5..M......1
-5.........1
+62.M.....48
+48.......62
 5.........1
 67772D47778
 '''
@@ -243,8 +243,8 @@ D.........D
 '''
 
 EVIL_ROOM_6 = '''
-43338D63332
-5.........1
+43338D6332 
+5........62
 5.........1
 5.........1
 8.........6
@@ -252,8 +252,8 @@ D.........D
 2.........4
 5..M......1
 5.........1
-5.........1
-67772D47778
+62........1
+ 6772D47778
 '''
 
 EVIL_ROOM_7 = '''
@@ -271,22 +271,22 @@ D.........D
 '''
 
 EVIL_ROOM_8 = '''
-43338D63332
-5.........1
-5.........1
+43248D62432
+5.15...15.1
+5.68...68.1
 5......M..1
 8.........6
 D.........D
 2.........4
 5.........1
-5.........1
-5.........1
-67772D47778
+5.42...42.1
+5.15...15.1
+67862D48678
 '''
 
 EVIL_ROOM_9 = '''
-43338D63332
-5.........1
+ 4338D6332 
+48.......62
 5.........1
 5.........1
 8.........6
@@ -294,22 +294,22 @@ D.........D
 2.........4
 5.....M...1
 5.........1
-5.........1
-67772D47778
+62.......48
+ 6772D4778 
 '''
 
 EVIL_ROOM_10 = '''
-43338D63332
-5.........1
-5.........1
+  438D632  
+  5.....1  
+438.....672
 5.........1
 8.........6
 D.........D
 2.........4
 5.........1
-5.........1
-5.........1
-67772D47778
+672.....478
+  5.....1  
+  672D478  
 '''
 
 EVIL_ROOM_11 = '''
@@ -369,21 +369,21 @@ D.........D
 '''
 
 EVIL_ROOM_15 = '''
-43338D63332
-5.........1
-5.........1
-5.........1
-8.........6
+   48D62   
+   5...1   
+   62.48   
+432 5.1 432
+8.638.638.6
 D.........D
-2.........4
-5.........1
-5.........1
-5.........1
-67772D47778
+2.472.472.4
+678 5.1 678
+   48.62   
+   5...1   
+   62D48   
 '''
 
 DOUBLE_EVIL_ROOM_1 = '''
-4333333333333333333332
+43338D6333333338D63332
 5....................1
 5....................1
 5..42.....42.....42..1
@@ -393,11 +393,11 @@ D....................D
 5..68.....68.....68..1
 5....................1
 5....................1
-6777777777777777777778
+67772D4777777772D47778
 '''
 
 DOUBLE_EVIL_ROOM_2 = '''
-4333333333333333333332
+43338D6333333338D63332
 5....................1
 5.........M..........1
 5..M.............M...1
@@ -407,11 +407,11 @@ D........C.C.........D
 5..M.............M...1
 5.........M..........1
 5....................1
-6777777777777777777778
+67772D4777777772D47778
 '''
 
 DOUBLE_EVIL_ROOM_3 = '''
-4333333333333333333332
+43338D6333333338D63332
 5....................1
 5.M..................1
 5....................1
@@ -421,21 +421,21 @@ D........5  1........D
 5....................1
 5..................M.1
 5....................1
-6777777777777777777778
+67772D4777777772D47778
 '''
 
 LOOT_ROOM_1 = '''
-43338D63332
-5.........1
-5.........1
-5.........1
-8.........6
+   48D62   
+   5...1   
+   62.48   
+43248.62432
+8.68...68.6
 D....C....D
-2.........4
-5.........1
-5.........1
-5.........1
-67772D47778
+2.42...42.4
+67862.48678
+   48.62   
+   5...1   
+   62D48   
 '''
 
 LOOT_ROOM_2 = '''
@@ -453,17 +453,17 @@ D...C.C...D
 '''
 
 LOOT_ROOM_3 = '''
-43338D63332
-5C........1
-5.........1
-5.........1
-8.........6
+43338D632  
+5C......632
+67772.....1
+43325.....1
+8..68.....6
 D.........D
-2.........4
-5.........1
-5.........1
-5........C1
-67772D47778
+2.....42..4
+5.....16778
+5.....63332
+672......C1
+  672D47778
 '''
 
 LOOT_ROOM_4 = '''
@@ -550,47 +550,71 @@ DOUBLE_ROOMS = [DOUBLE_EVIL_ROOM_1, DOUBLE_EVIL_ROOM_2, DOUBLE_EVIL_ROOM_3,
 
 PORTAL_ROOMS = [START_ROOM, END_ROOM]
 
-TEST_LEVEL = '''SDD'''
+LITTLE_CHANCE = [1, 0, 0, 0, 0, 0, 0]
+CHANCE = [1, 0, 0]
+BIG_CHANCE = [1, 1, 0]
 
+TEST_LEVEL = '''
+SDDDDDDDDDDDDDD
+             DD
+'''
 
-LEVEL_1 = '''   RR RRR RR 
+LEVEL_1 = '''
+  RR RRR RR  
  RSRR RRRRR  
 RRR RRRR RRE 
-RDDRR RR RRRR
-RRR RDDRR    '''
+ DDRR RR RRRR
+RRR RDDRR    
+  RR R RRRRR 
+  '''    # 50
 
-LEVEL_2 = '''   RR RSR RR 
+LEVEL_2 = '''
+   RR RSR RR 
  RRRR R RRRRR
+RRR  RRRR   R
 RR RRR    RRR
  RR RR RDDR  
-RER RRRRR    '''
+RER RRRRR    
+'''      # 50
 
-LEVEL_3 = '''RDDRRER
-RR R RR
-RRR RRR
-RR R RR
-SRRRDDR'''
+LEVEL_3 = '''
+ RR   R   RR 
+  RRDDRRERR  
+RRR  RRR  RRR
+  RRR   RRR  
+RRR  RRR  RRR
+  RSRRRDDRR  
+ RR   R   RR 
+'''      # 50
 
-LEVEL_4 = '''   RR RRR RS 
- R  R RRRRR  
-RR RRR R RRR 
-RDDRR  R RRRE
-RRR RRRRRR   '''
+LEVEL_4 = '''
+RRRR RRR RS  
+ R  R RRRRR R
+RR RRR R RRRR
+R R R  R  RR 
+ DDRR  R RRRE
+RRR RRRRRR  R
+'''      # 50
 
-LEVEL_5 = '''RRDDRRSRR
-R   R   R
-RRRRRRRRR
-R   R   R
-ERRRRDDRR'''
+LEVEL_5 = '''
+ R   RR RRRER
+ RR R RRR R  
+  RRRRR RRR  
+RRR  R R R RR
+R R RRRRRRRR 
+  RSR R   R  
+   R RR      
+'''
 
 FORMS = [LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5]
 
 
 # Сама функция генерации
-def generate_new_level():
+def generate_new_level() -> [str, ..., str]:
     level_form = choice(FORMS)
+    level_form = LEVEL_5
 
-    level_form = level_form.split('\n')
+    level_form = level_form.strip('\n').split('\n')
     level = []
     doubled = False
     for i in range(len(level_form)):
@@ -616,20 +640,116 @@ def generate_new_level():
             rooms.append(room.strip('\n').split('\n'))
 
         for numder_row in range(len(rooms[0])):
-            row = ''
+            row = []
             for room_a in rooms:
                 print(room_a)
-                row += room_a[numder_row]
+                row += list(room_a[numder_row])
             level.append(row)
 
-    pprint(level)
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] != 'D':
+                continue
 
+            if level[i - 1][j] == '6' and level[i + 1][j] == '4':
+                if j + 1 >= len(level[i]) or level[i][j + 1] not in 'Dlrtb':
+                    if level[i + 2][j - 2] == '.' and choice(LITTLE_CHANCE):
+                        level[i - 1][j] = '8'
+                        level[i + 1][j] = '2'
+                        level[i][j] = ' '
+
+                        if choice(CHANCE):
+                            level[i][j - 1] = ' '
+                            level[i - 1][j - 1] = '7'
+                            level[i + 1][j - 1] = '3'
+                            level[i][j - 2] = '1'
+                            level[i - 1][j - 2] = '4'
+                            level[i + 1][j - 2] = '6'
+                        else:
+                            level[i][j - 1] = '1'
+                            level[i - 1][j - 1] = '4'
+                            level[i + 1][j - 1] = '6'
+                    else:
+                        level[i][j] = level[i - 1][j] = level[i + 1][j] = '1'
+                else:
+                    level[i][j] = 'r'
+
+            elif level[i][j - 1] == '8' and level[i][j + 1] == '6':
+                if i == 0 or level[i - 1][j] not in 'Dlrtb':
+                    if level[i + 2][j + 2] == '.' and choice(LITTLE_CHANCE):
+                        level[i][j + 1] = '4'
+                        level[i][j - 1] = '2'
+                        level[i][j] = ' '
+
+                        if choice(CHANCE):
+                            level[i + 2][j] = '3'
+                            level[i + 2][j - 1] = '6'
+                            level[i + 2][j + 1] = '8'
+                            level[i + 1][j] = ' '
+                            level[i + 1][j - 1] = '1'
+                            level[i + 1][j + 1] = '5'
+                        else:
+                            level[i + 1][j] = '3'
+                            level[i + 1][j - 1] = '6'
+                            level[i + 1][j + 1] = '8'
+                    else:
+                        level[i][j] = level[i][j - 1] = level[i][j + 1] = '3'
+                else:
+                    level[i][j] = 'b'
+
+            elif level[i - 1][j] == '8' and level[i + 1][j] == '2':
+                if j == 0 or level[i][j - 1] not in 'Dlrtb':
+                    if level[i + 2][j + 2] == '.' and choice(LITTLE_CHANCE):
+                        level[i - 1][j] = '6'
+                        level[i + 1][j] = '4'
+                        level[i][j] = ' '
+
+                        if choice(CHANCE):
+                            level[i][j + 2] = '5'
+                            level[i - 1][j + 2] = '2'
+                            level[i + 1][j + 2] = '8'
+                            level[i][j + 1] = ' '
+                            level[i - 1][j + 1] = '3'
+                            level[i + 1][j + 1] = '7'
+                        else:
+                            level[i][j + 1] = '5'
+                            level[i - 1][j + 1] = '2'
+                            level[i + 1][j + 1] = '8'
+                    else:
+                        level[i][j] = level[i - 1][j] = level[i + 1][j] = '5'
+                else:
+                    level[i][j] = 'l'
+
+            elif level[i][j - 1] == '2' and level[i][j + 1] == '4':
+                if i + 1 >= len(level) or level[i + 1][j] not in 'Dlrtb':
+                    if level[i - 2][j + 2] == '.' and choice(LITTLE_CHANCE):
+                        level[i][j + 1] = '6'
+                        level[i][j - 1] = '8'
+                        level[i][j] = ' '
+
+                        if choice(CHANCE):
+                            level[i - 2][j] = '7'
+                            level[i - 2][j - 1] = '4'
+                            level[i - 2][j + 1] = '2'
+                            level[i - 1][j] = ' '
+                            level[i - 1][j - 1] = '1'
+                            level[i - 1][j + 1] = '5'
+                        else:
+                            level[i - 1][j] = '7'
+                            level[i - 1][j - 1] = '4'
+                            level[i - 1][j + 1] = '2'
+                    else:
+                        level[i][j] = level[i][j - 1] = level[i][j + 1] = '7'
+                else:
+                    level[i][j] = 't'
+
+    pprint(level)
     # Возвращаем созданный уровень
     return [''.join(i) for i in level]
 
 
 FPS = 50
-EMPTY = '.@DMPCH'
+EMPTY = ' .@DMPCHLTlrtb' + '12345678'
 WALL = '#'
 
 # группы спрайтов
@@ -642,7 +762,7 @@ if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Ла-ла-ла')
     clock = pygame.time.Clock()
-    size = width, height = 850, 850
+    size = width, height = 1450, 1050
     tile_width = tile_height = 50
     screen = pygame.display.set_mode(size)
 
@@ -655,12 +775,13 @@ if __name__ == '__main__':
 
     camera = Camera()
 
-    m1 = list('12345678DMPCHLTE ') + ['.', '.0', '.1', '.2', '.3']
+    m1 = list('12345678DrltbMPCHLTE ') + ['.', '.0', '.1', '.2', '.3']
     m2 = ['RIGHT_WALL.png', 'TOP_RIGHT_WALL.png', 'TOP_WALL.png', 'TOP_LEFT_WALL.png',
           'LEFT_WALL.png', 'DOWN_LEFT_WALL.png', 'DOWN_WALL.png', 'DOWN_RIGHT_WALL.png',
-          'DOOR_VERTICAL.png', 'EMPTY.png', 'EMPTY.png', 'EMPTY.png', 'EMPTY.png',
-          'EMPTY.png', 'EMPTY.png', 'EMPTY.png', 'EMPTY.png', 'FLOOR.png', 'FLOOR_CRACKED_0.png',
-          'FLOOR_CRACKED_1.png', 'FLOOR_CRACKED_2.png', 'FLOOR_CRACKED_3.png']
+          'DOOR_VERTICAL.png', 'DOOR_RIGHT.png', 'DOOR_LEFT.png', 'DOOR_TOP.png', 'DOOR_BOTTOM.png',
+          'EMPTY.png', 'EMPTY.png', 'EMPTY.png', 'EMPTY.png', 'EMPTY.png', 'EMPTY.png', 'EMPTY.png',
+          'DARK.png', 'FLOOR.png', 'FLOOR_CRACKED_0.png', 'FLOOR_CRACKED_1.png', 'FLOOR_CRACKED_2.png',
+          'FLOOR_CRACKED_3.png']
 
     tile_images = {m1[i]: load_image(m2[i]) for i in range(len(m1))}
 
