f1ab47685cfdd924e5d0012f3efdb7f31f74472c
Massering <74819874+Massering@users.noreply.github.com>
2021-01-14 16:44:55 +0700

    Подключение вертикальной синхронизации

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     from UI import start_screen
 
     # Экран (он же будет использован везде)
-    screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
+    screen = pygame.display.set_mode(flags=pygame.FULLSCREEN, vsync=True)
 
     # этот модуль нужно импортировать именно тут,
     # т.к. в нём происходит загрузка изображений (а это можно делать только
