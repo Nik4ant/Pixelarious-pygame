700fcbff2ef0a8a15b2e64a24e34f943164b04ef
Massering <74819874+Massering@users.noreply.github.com>
2021-01-22 00:11:15 +0700

    Много новых классов, устройство кода

--- entities/enemies.py
+++ entities/enemies.py
@@ -70,7 +70,7 @@ class WalkingMonster(Entity):
             point_x, point_y = self.point
             line = max(((point_x - self_x) ** 2 + (point_y - self_y) ** 2) ** 0.5, self.speed)
 
-            part_move = max(line / self.speed, 1)
+            part_move = max(line / self.speed, 0.5)
             self.dx = (point_x - self_x) / part_move
             self.dy = (point_y - self_y) / part_move
             if line > 1.5 * TILE_SIZE:
@@ -78,7 +78,7 @@ class WalkingMonster(Entity):
                 self.dy *= 3
 
         else:
-            part_move = max(line / self.speed, 10)
+            part_move = max(line / self.speed, 1)
             self.dx = (point_x - self_x) * 4 / part_move
             self.dy = (point_y - self_y) * 4 / part_move
 
@@ -103,6 +103,11 @@ class WalkingMonster(Entity):
             super().update()
 
 
+# TODO: Shooting class must be initialized here
+class ShootingMonster(Entity):
+    pass
+
+
 class Demon(WalkingMonster):
     frames = cut_sheet(load_image('demon_run.png', 'assets\\enemies'), 4, 2)
 
@@ -120,7 +125,7 @@ class Demon(WalkingMonster):
 
     def __init__(self, x, y, *args):
         super().__init__(x, y, *args)
-        self.speed = TILE_SIZE * 0.025
+        self.speed = TILE_SIZE * 0.03
         self.visibility_range = TILE_SIZE * 7
 
 
@@ -145,11 +150,129 @@ class GreenSlime(WalkingMonster):
         self.visibility_range = TILE_SIZE * 5
 
 
+class DirtySlime(WalkingMonster):
+    frames = cut_sheet(load_image('dirty_slime_any.png', 'assets\\enemies'), 4, 2)
+
+    look_directions = {
+        (-1, -1): 1,
+        (-1, 0): 1,
+        (-1, 1): 1,
+        (0, -1): 1,
+        (0, 0): 2,
+        (0, 1): 0,
+        (1, -1): 0,
+        (1, 0): 0,
+        (1, 1): 0
+    }
+
+    def __init__(self, x, y, *args):
+        super().__init__(x, y, *args)
+        self.speed = TILE_SIZE * 0.02
+        self.visibility_range = TILE_SIZE * 5
+
+
+class Zombie(WalkingMonster):
+    frames = cut_sheet(load_image('zombie_run.png', 'assets\\enemies'), 4, 2)
+
+    look_directions = {
+        (-1, -1): 1,
+        (-1, 0): 1,
+        (-1, 1): 1,
+        (0, -1): 1,
+        (0, 0): 2,
+        (0, 1): 0,
+        (1, -1): 0,
+        (1, 0): 0,
+        (1, 1): 0
+    }
+
+    def __init__(self, x, y, *args):
+        super().__init__(x, y, *args)
+        self.speed = TILE_SIZE * 0.025
+        self.visibility_range = TILE_SIZE * 5
+
+
+# TODO: Shooting class must be here
+class Wizard(WalkingMonster):
+    frames = cut_sheet(load_image('wizard_run.png', 'assets\\enemies'), 4, 2)
+
+    look_directions = {
+        (-1, -1): 1,
+        (-1, 0): 1,
+        (-1, 1): 1,
+        (0, -1): 1,
+        (0, 0): 2,
+        (0, 1): 0,
+        (1, -1): 0,
+        (1, 0): 0,
+        (1, 1): 0
+    }
+
+    def __init__(self, x, y, *args):
+        super().__init__(x, y, *args)
+        self.speed = TILE_SIZE * 0.025
+        self.visibility_range = TILE_SIZE * 5
+
+
+# TODO: Shooting class must be here
+class LongWizard(WalkingMonster):
+    frames = cut_sheet(load_image('long_wizard_run.png', 'assets\\enemies'), 4, 2)
+
+    look_directions = {
+        (-1, -1): 1,
+        (-1, 0): 1,
+        (-1, 1): 1,
+        (0, -1): 1,
+        (0, 0): 2,
+        (0, 1): 0,
+        (1, -1): 0,
+        (1, 0): 0,
+        (1, 1): 0
+    }
+
+    def __init__(self, x, y, *args):
+        super().__init__(x, y, *args)
+        self.speed = TILE_SIZE * 0.025
+        self.visibility_range = TILE_SIZE * 5
+
+
+class Skeleton(WalkingMonster):
+    size = (int(TILE_SIZE * 1.5),) * 2
+    frames = cut_sheet(load_image('skelet.png', 'assets\\enemies'), 4, 4, size)
+
+    look_directions = {
+        (-1, -1): 1,
+        (-1, 0): 1,
+        (-1, 1): 1,
+        (0, -1): 3,
+        (0, 0): 2,
+        (0, 1): 0,
+        (1, -1): 2,
+        (1, 0): 2,
+        (1, 1): 2
+    }
+
+    def __init__(self, x, y, *args):
+        super().__init__(x, y, *args)
+        self.speed = TILE_SIZE * 0.03
+        self.visibility_range = TILE_SIZE * 7
+
+
 def random_monster(x, y, all_sprites, enemies_group):
-    n = randint(1, 2)
+    n = randint(1, 7)
     args = (x * TILE_SIZE + TILE_SIZE * 0.5, y * TILE_SIZE + TILE_SIZE * 0.5,
             all_sprites, enemies_group)
     if n == 1:
         return Demon(*args)
     elif n == 2:
         return GreenSlime(*args)
+    elif n == 3:
+        return DirtySlime(*args)
+    elif n == 4:
+        return Zombie(*args)
+    elif n == 5:
+        return Wizard(*args)
+    elif n == 6:
+        return LongWizard(*args)
+    elif n == 7:
+        return Skeleton(*args)
